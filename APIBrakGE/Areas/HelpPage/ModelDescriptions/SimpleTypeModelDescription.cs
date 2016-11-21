338ce6a93abf48d0a90cc00a8ecd058dc3b386c1
Yeiner Andres <yeanmagu@gmail.com>
2016-11-21 11:08:28 -0500

    Agregar archivos de proyecto.

---
+++ APIBrakGE/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIBrakGE.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ APIBrakGE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIBrakGE.WebApiApplication" Language="C#" %>
